5f573e172392edb2d3a056467e6b167a16291d21
Minna <Minna.Yao@gmail.com>
2012-12-09 23:03:16 +1000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Patapet::Application.config.secret_token = '6609a9210d9926013936a8716242188cf06d255a7e3a24ee1c86f2e8fd1c1fcf4f7b9859bb0ecab3744fa4ffd243603e8c2c3c5548e69e826ac316f9ca484dbd'
